85e59194d6a0cee1f3687a669ff5ffbbc63dd825
Muhammad Sorour <madasorour1@gmail.com>
2020-12-12 03:14:57 +0200

    added CountDownTimer instead of Timer added exit Button to exit from recent apps made a validation on Edittext added stop button to stop the service

--- app/src/main/java/com/example/countdownclosewifi/MyService.java
+++ app/src/main/java/com/example/countdownclosewifi/MyService.java
@@ -5,26 +5,31 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
-//import android.content.BroadcastReceiver;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Build;
+import android.os.CountDownTimer;
 import android.os.IBinder;
-import android.util.Log;
+
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 import com.example.countdownclosewifi.UI.MainActivity;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Locale;
+
 
 public class MyService extends Service {
     private WifiManager wifiManager;
     private int timeRemaining;
-    private Timer timer;
+
+    private static final String channel_id = "Notification Channel";
+    private BroadcastReceiver broadcastReceiver;
+    private CountDownTimer countDownTimer;
 
     @Nullable
     @Override
@@ -34,83 +39,147 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        //make notification channel to show notification if os is higher than android Oreo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "notification";
+            NotificationChannel notificationChannel = new NotificationChannel(channel_id, name, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("Stop");
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                countDownTimer.cancel();
+            }
+        };
+        registerReceiver(broadcastReceiver, intentFilter);
+
 
         timeRemaining = intent.getIntExtra("Time", 0);
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            private static final String TAG = "sss";
+
+        countDownTimer = new CountDownTimer(timeRemaining + 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                int minutes = (int) ((millisUntilFinished / (1000 * 60)) % 60);
+                int hours = (int) ((millisUntilFinished / (1000 * 60 * 60)) % 24);
+                String timeLeftFormatted = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
+                showNotification(timeLeftFormatted);
+                Intent intent = new Intent();
+                intent.setAction("Count");
+                intent.putExtra("Show", timeLeftFormatted);
+                sendBroadcast(intent);
+                timeRemaining -= 1000;
+            }
 
             @Override
-            public void run() {
-
-                if (timeRemaining == 0) {
-                    wifiManager.setWifiEnabled(false);
-                    timer.cancel();
-                    stopSelf();
-                }
-
-                showNotification(timeRemaining);
-
-                Log.e(TAG, "run: test");
-
+            public void onFinish() {
+                wifiManager.setWifiEnabled(false);
                 Intent intent = new Intent();
                 intent.setAction("Count");
                 intent.putExtra("Show", timeRemaining);
                 sendBroadcast(intent);
-
-                timeRemaining -= 1;
-
-
+                stopSelf();
             }
-        }, 0, 1000);
+        }.start();
+
+//        timer = new Timer();
+//        timer.scheduleAtFixedRate(new TimerTask() {
+//
+//            @Override
+//            public void run() {
+//
+//                if (timeRemaining == 0) {
+//                    wifiManager.setWifiEnabled(false);
+//                    timer.cancel();
+//                    stopSelf();
+//                }
+//
+//
+////                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+////                    startMyOwnForeground(timeRemaining);
+////                else
+////                    startForeground(1, new Notification());
+//
+//                showNotification(timeRemaining);
+//                Intent intent = new Intent();
+//                intent.setAction("Count");
+//                intent.putExtra("Show", timeRemaining);
+//                sendBroadcast(intent);
+//
+//                timeRemaining -= 1;
+//
+//            }
+//        }, 0, 1000);
 
         return START_STICKY;
     }
 
 
-    private void showNotification(int timeRemaining) {
+//    private void startMyOwnForeground(int timeRemaining) {
+//        String NOTIFICATION_CHANNEL_ID = "com.example.simpleapp";
+//        String channelName = "My Background Service";
+//        NotificationChannel chan = null;
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+//            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//            assert manager != null;
+//            manager.createNotificationChannel(chan);
+//        }
+//
+//
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+//        Notification notification = notificationBuilder
+//                .setSmallIcon(R.drawable.add_circle_24)
+//                .setContentTitle("App is running in background")
+////                .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+////                .setCategory(Notification.CATEGORY_SERVICE)
+//                .setContentText(String.valueOf(timeRemaining))
+////                .setOnlyAlertOnce(true)
+//                .build();
+//        startForeground(2, notification);
+//    }
+
+
+    private void showNotification(String timeLeftFormatted) {
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent =
                 PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        String channel_id = "id";
         Notification notification;
-        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O) {
+        // VERY IMPORTANT *** Notification ID must be same as Notification Channel ID
+        // DON'T FORGET THAT
+        // NEVER
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             notification = new Notification.Builder(this, channel_id)
                     .setContentTitle("You are running out of time")
-                    .setContentText(String.valueOf(timeRemaining))
+                    .setContentText(timeLeftFormatted)
+//                    .setCategory(Notification.CATEGORY_SERVICE)
                     .setSmallIcon(R.drawable.wifi_24)
                     .setContentIntent(pendingIntent)
                     .setOnlyAlertOnce(true)
+//                    .setOngoing(true)
                     .build();
         } else {
             notification = new NotificationCompat.Builder(this, channel_id)
                     .setContentTitle("You are running out of time")
-                    .setContentText(String.valueOf(timeRemaining))
+                    .setContentText(timeLeftFormatted)
                     .setSmallIcon(R.drawable.wifi_24)
                     .setContentIntent(pendingIntent)
                     .setOnlyAlertOnce(true)
+                    .setOngoing(true)
                     .build();
         }
 
         // Notification ID cannot be 0.
-       startForeground(12,notification);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String id = "Notification Channel";
-            CharSequence name = "notification";
-            NotificationChannel notificationChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-
-
+        startForeground(12, notification);
 
     }
-
 
 
     @Override
@@ -118,5 +187,6 @@ public class MyService extends Service {
         super.onDestroy();
         stopForeground(true);
         stopSelf();
+        unregisterReceiver(broadcastReceiver);
     }
 }
--- app/src/main/java/com/example/countdownclosewifi/UI/MainActivity.java
+++ app/src/main/java/com/example/countdownclosewifi/UI/MainActivity.java
@@ -9,21 +9,27 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.Toast;
 
 import com.example.countdownclosewifi.MyService;
 import com.example.countdownclosewifi.R;
 import com.example.countdownclosewifi.databinding.ActivityMainBinding;
 
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private BroadcastReceiver receiver;
+    private Vibrator v;
+    private String timeFormat;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         // Intent filter to set the same action of the broadcast sender
         IntentFilter intentFilter_getRemainingTime = new IntentFilter();
@@ -33,19 +39,26 @@ public class MainActivity extends AppCompatActivity {
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                int i = intent.getIntExtra("Show", 0);
-                binding.timeLeft.setText(String.valueOf(i));
+                timeFormat = intent.getStringExtra("Show");
+                binding.timeLeft.setText(timeFormat);
             }
         };
         registerReceiver(receiver, intentFilter_getRemainingTime);
 
 
         binding.start.setOnClickListener(view -> {
-            if (binding.setTime.getText().toString().isEmpty() || Long.parseLong(binding.setTime.getText().toString()) == 0) {
+            String hours = binding.setTimeHours.getText().toString();
+            String minutes = binding.setTimeMinutes.getText().toString();
+
+            int ihours, iminutes;
+            if (minutes.equals("0") && hours.equals("0") || minutes.isEmpty() && hours.isEmpty()) {
                 Toast.makeText(MainActivity.this, "Enter the duration", Toast.LENGTH_SHORT).show();
-            } else {
+            } else if (!minutes.isEmpty() && !minutes.equals("0") && !hours.isEmpty() && !hours.equals("0")) {
+                ihours = Integer.parseInt(binding.setTimeHours.getText().toString());
+                iminutes = Integer.parseInt(binding.setTimeMinutes.getText().toString());
+
                 Intent serviceIntent = new Intent(this, MyService.class);
-                int time = Integer.parseInt(binding.setTime.getText().toString());
+                int time = ihours * 60 * 60 * 1000 + iminutes * 60 * 1000;
                 serviceIntent.putExtra("Time", time);
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     startForegroundService(serviceIntent);
@@ -53,7 +66,60 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     startService(serviceIntent);
                 }
+            } else if (!minutes.isEmpty() && !minutes.equals("0")) {
+                iminutes = Integer.parseInt(binding.setTimeMinutes.getText().toString());
+                Intent serviceIntent = new Intent(this, MyService.class);
+                int time = iminutes * 60 * 1000;
+                serviceIntent.putExtra("Time", time);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent);
 
+                } else {
+                    startService(serviceIntent);
+                }
+            } else if (!hours.isEmpty() && !hours.equals("0")) {
+                ihours = Integer.parseInt(binding.setTimeHours.getText().toString());
+                Intent serviceIntent = new Intent(this, MyService.class);
+                int time = ihours * 60 * 60 * 1000;
+                serviceIntent.putExtra("Time", time);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent);
+
+                } else {
+                    startService(serviceIntent);
+                }
+            }
+
+        });
+
+        binding.stop.setOnClickListener(view -> {
+            if (timeFormat != null) {
+                Intent stop = new Intent();
+                stop.setAction("Stop");
+                sendBroadcast(stop);
+
+                Intent serviceIntent = new Intent(MainActivity.this, MyService.class);
+                stopService(serviceIntent);
+
+                // Vibrate for 500 milliseconds
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    //deprecated in API 26
+                    v.vibrate(500);
+                }
+            } else {
+                Toast.makeText(this, "Service is not running", Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+        binding.exit.setOnClickListener(view -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                finishAndRemoveTask();
+            } else {
+                finish();
             }
 
         });
